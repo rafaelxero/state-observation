c120d7865329a4ed5c32641651af94fd8026ba23
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2017-06-20 21:18:28 +0900

    Add the appropriate include in the relevant place

--- unit-testings/test_imu-elastic-local-frame-dynamical-system.cpp
+++ unit-testings/test_imu-elastic-local-frame-dynamical-system.cpp
@@ -4,6 +4,7 @@
 #include <state-observation/noise/gaussian-white-noise.hpp>
 #include <state-observation/examples/offline-ekf-flexibility-estimation.hpp>
 #include <state-observation/dynamical-system/dynamical-system-simulator.hpp>
+#include <state-observation/flexibility-estimation/model-base-ekf-flex-estimator-imu.hpp>
 #include <state-observation/tools/miscellaneous-algorithms.hpp>
 
 #include <time.h>
