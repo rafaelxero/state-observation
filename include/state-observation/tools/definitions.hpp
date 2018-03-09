d66602ad26731e55540f98a69f8f87e0404013d2
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2018-03-09 16:43:11 +0900

    Improve indentation

--- include/state-observation/tools/definitions.hpp
+++ include/state-observation/tools/definitions.hpp
@@ -35,47 +35,47 @@
 
 namespace stateObservation
 {
-    ///Dynamic sized scalar vector
-    typedef Eigen::VectorXd Vector;
+  ///Dynamic sized scalar vector
+  typedef Eigen::VectorXd Vector;
 
-    ///3D vector
-    typedef Eigen::Vector3d Vector3;
+  ///3D vector
+  typedef Eigen::Vector3d Vector3;
 
-    ///3D vector unaligned
-    typedef Eigen::Matrix<double, 3, 1, Eigen::DontAlign> Vector3Unaligned;
+  ///3D vector unaligned
+  typedef Eigen::Matrix<double, 3, 1, Eigen::DontAlign> Vector3Unaligned;
 
-    ///4D vector
-    typedef Eigen::Vector4d Vector4;
+  ///4D vector
+  typedef Eigen::Vector4d Vector4;
 
-    /// 6D vector
-    typedef Eigen::Matrix<double,6,1> Vector6;
+  /// 6D vector
+  typedef Eigen::Matrix<double,6,1> Vector6;
 
-    ///Dynamic sized Matrix
-    typedef Eigen::MatrixXd Matrix;
+  ///Dynamic sized Matrix
+  typedef Eigen::MatrixXd Matrix;
 
-    ///3x3 Scalar Matrix
-    typedef Eigen::Matrix3d Matrix3;
+  ///3x3 Scalar Matrix
+  typedef Eigen::Matrix3d Matrix3;
 
-    ///6x6 Scalar Matrix
-    typedef Eigen::Matrix<double, 6, 6> Matrix6;
+  ///6x6 Scalar Matrix
+  typedef Eigen::Matrix<double, 6, 6> Matrix6;
 
-    ///3x3 Scalar Matrix Unaligned
-    typedef Eigen::Matrix<double, 3, 3, Eigen::DontAlign> Matrix3Unaligned;
+  ///3x3 Scalar Matrix Unaligned
+  typedef Eigen::Matrix<double, 3, 3, Eigen::DontAlign> Matrix3Unaligned;
 
-    ///3x3 Scalar Matrix
-    typedef Eigen::Matrix3d Matrix3;
+  ///3x3 Scalar Matrix
+  typedef Eigen::Matrix3d Matrix3;
 
-    ///4x4 Scalar Matrix
-    typedef Eigen::Matrix4d Matrix4;
+  ///4x4 Scalar Matrix
+  typedef Eigen::Matrix4d Matrix4;
 
-    ///Quaternion
-    typedef Eigen::Quaterniond Quaternion;
+  ///Quaternion
+  typedef Eigen::Quaterniond Quaternion;
 
-    ///Quaternion Unaligned
-    typedef Eigen::Quaternion<double, Eigen::DontAlign> QuaternionUnaligned;
+  ///Quaternion Unaligned
+  typedef Eigen::Quaternion<double, Eigen::DontAlign> QuaternionUnaligned;
 
-    ///Euler Axis/Angle representation of orientation
-    typedef Eigen::AngleAxis<double> AngleAxis;
+  ///Euler Axis/Angle representation of orientation
+  typedef Eigen::AngleAxis<double> AngleAxis;
 
 #undef SO_DEBUG_ONLY(expr)
 #ifndef NDEBUG
@@ -246,153 +246,153 @@ namespace stateObservation
 
   typedef IndexedMatrixT<> IndexedMatrix;
 
-    /**
-     * \class    IndexedMatrix
-     * \brief    This class describes a structure that enables to store array of matrices
-     *           with time indexation.
-     *
-     */
+  /**
+   * \class    IndexedMatrix
+   * \brief    This class describes a structure that enables to store array of matrices
+   *           with time indexation.
+   *
+   */
 
-    class IndexedMatrixArray
-    {
-        public:
-        ///Default constructor
-        IndexedMatrixArray();
+  class IndexedMatrixArray
+  {
+  public:
+    ///Default constructor
+    IndexedMatrixArray();
 
-        ///Sets the vector v at the time index k
-        ///It checks the time index, the array must have contiguous indexes
-        ///It can be used to push a value into the back of the array
-        inline void setValue(const Matrix& v,unsigned k);
+    ///Sets the vector v at the time index k
+    ///It checks the time index, the array must have contiguous indexes
+    ///It can be used to push a value into the back of the array
+    inline void setValue(const Matrix& v,unsigned k);
 
-        ///Pushes back the matrix to the array, the new value will take the next time
-        inline void pushBack(const Matrix& v);
+    ///Pushes back the matrix to the array, the new value will take the next time
+    inline void pushBack(const Matrix& v);
 
-        ///removes the first (oldest) element of the array
-        inline void popFront();
+    ///removes the first (oldest) element of the array
+    inline void popFront();
 
-        ///gets the value with the given time index
-        inline Matrix operator[](unsigned index) const;
+    ///gets the value with the given time index
+    inline Matrix operator[](unsigned index) const;
 
-        ///gets the value with the given time index, non const version
-        inline Matrix  & operator[](unsigned index);
+    ///gets the value with the given time index, non const version
+    inline Matrix  & operator[](unsigned index);
 
-        ///gets the first value
-        inline const Matrix & front() const;
+    ///gets the first value
+    inline const Matrix & front() const;
 
-        ///gets the first value
-        inline Matrix& front();
+    ///gets the first value
+    inline Matrix& front();
 
-        ///gets the last value
-        inline const Matrix & back() const;
+    ///gets the last value
+    inline const Matrix & back() const;
 
-        ///gets the last value
-        inline Matrix & back();
+    ///gets the last value
+    inline Matrix & back();
 
-        ///removes all the elements with larger or equal indexes than timeIndex
-        void truncate(unsigned timeIndex);
+    ///removes all the elements with larger or equal indexes than timeIndex
+    void truncate(unsigned timeIndex);
 
-        ///resizes the array
-        inline void resize(unsigned i, const Matrix & m= Matrix::Zero(0,0));
+    ///resizes the array
+    inline void resize(unsigned i, const Matrix & m= Matrix::Zero(0,0));
 
-        ///Get the time index
-        inline int getLastIndex() const;
+    ///Get the time index
+    inline int getLastIndex() const;
 
-        ///Get the time index of the next value that will be pushed back
-        /// Can be used in for loops
-        inline unsigned getNextIndex() const;
+    ///Get the time index of the next value that will be pushed back
+    /// Can be used in for loops
+    inline unsigned getNextIndex() const;
 
-        ///Set the time index of the last element
-        inline unsigned setLastIndex(int index);
+    ///Set the time index of the last element
+    inline unsigned setLastIndex(int index);
 
-        ///Get the time index
-        inline unsigned getFirstIndex() const;
+    ///Get the time index
+    inline unsigned getFirstIndex() const;
 
-        ///set the time index of the first element
-        inline unsigned setFirstIndex(int index);
+    ///set the time index of the first element
+    inline unsigned setFirstIndex(int index);
 
-        inline unsigned size() const;
+    inline unsigned size() const;
 
-        ///Resets the array to initial state
-        ///the value is no longer accessible
-        inline void reset();
+    ///Resets the array to initial state
+    ///the value is no longer accessible
+    inline void reset();
 
-        ///Clears the vector but keeps the last index
-        inline void clear();
+    ///Clears the vector but keeps the last index
+    inline void clear();
 
-        ///converts the array into a standard vector
-        std::vector<Matrix> getArray() const;
+    ///converts the array into a standard vector
+    std::vector<Matrix> getArray() const;
 
-        ///checks whether the index is present in the array
-        inline bool checkIndex(unsigned k) const;
+    ///checks whether the index is present in the array
+    inline bool checkIndex(unsigned k) const;
 
-        ///gets the array from a file
-        ///the line starts with the time index and then the matrix is read
-        ///row by row
-        ///WARNING: this resets the array
-        void readFromFile(const char * filename, size_t rows, size_t cols=1, bool withTimeStamp = true);
+    ///gets the array from a file
+    ///the line starts with the time index and then the matrix is read
+    ///row by row
+    ///WARNING: this resets the array
+    void readFromFile(const char * filename, size_t rows, size_t cols=1, bool withTimeStamp = true);
 
-        ///gets the array from a file
-        ///the line starts with the time index and then every line of the file
-        /// is converted into a vector
-        ///WARNING: this resets the array
-        void readVectorsFromFile(const char * filename, bool withTimeStamp = true );
+    ///gets the array from a file
+    ///the line starts with the time index and then every line of the file
+    /// is converted into a vector
+    ///WARNING: this resets the array
+    void readVectorsFromFile(const char * filename, bool withTimeStamp = true );
 
-        ///write the array in a a file
-        ///the line starts with the time index and then the matrix is described
-        ///row by row
-        /// When clear is set, the array is cleared but the time index is conserved
-        /// When append is set to true, the output is appended to file
-        void writeInFile(const char * filename, bool clear=false, bool append =false);
+    ///write the array in a a file
+    ///the line starts with the time index and then the matrix is described
+    ///row by row
+    /// When clear is set, the array is cleared but the time index is conserved
+    /// When append is set to true, the output is appended to file
+    void writeInFile(const char * filename, bool clear=false, bool append =false);
 
-        ///write the array in a a file
-        ///the line starts with the time index and then the matrix is described
-        ///row by row
-        /// When clear is set, the array is cleared but the time index is conserved
-        /// When append is set to true, the output is appended to file
-        void writeInFile(const std::string & filename, bool clear=false, bool append =false);
+    ///write the array in a a file
+    ///the line starts with the time index and then the matrix is described
+    ///row by row
+    /// When clear is set, the array is cleared but the time index is conserved
+    /// When append is set to true, the output is appended to file
+    void writeInFile(const std::string & filename, bool clear=false, bool append =false);
 
-    protected:
-        ///Asserts that the index is present in the array
-        ///does nothing in release mode
-        inline void check_(unsigned time) const;
+  protected:
+    ///Asserts that the index is present in the array
+    ///does nothing in release mode
+    inline void check_(unsigned time) const;
 
-        ///Asserts that the array is not empty
-        ///does nothing in release mode
-        inline void check_() const;
+    ///Asserts that the array is not empty
+    ///does nothing in release mode
+    inline void check_() const;
 
-        inline void checkNext_(unsigned time) const;
+    inline void checkNext_(unsigned time) const;
 
-        unsigned k_;
+    unsigned k_;
 
-        std::deque<Matrix> v_;
+    std::deque<Matrix> v_;
 
-    };
+  };
 
-    namespace kine
-    {
-        ///indexes of the different components of a vector of the kinematic state
-        const unsigned pos = 0;
-        const unsigned ori = 3;
-        const unsigned linVel = 6;
-        const unsigned angVel = 9;
-        const unsigned linAcc = 12;
-        const unsigned angAcc = 15;
-    }
+  namespace kine
+  {
+    ///indexes of the different components of a vector of the kinematic state
+    const unsigned pos = 0;
+    const unsigned ori = 3;
+    const unsigned linVel = 6;
+    const unsigned angVel = 9;
+    const unsigned linAcc = 12;
+    const unsigned angAcc = 15;
+  }
 
-    namespace cst
-    {
-        const double gravityConstant = 9.8;
+  namespace cst
+  {
+    const double gravityConstant = 9.8;
 
-        ///Gravity Vector along Z
-        const Vector gravity= gravityConstant * Vector3::UnitZ();
+    ///Gravity Vector along Z
+    const Vector gravity= gravityConstant * Vector3::UnitZ();
 
-        ///angles considered Zero
-        const double epsilonAngle=1e-16;
+    ///angles considered Zero
+    const double epsilonAngle=1e-16;
 
-    }
+  }
 
 
-    #include <state-observation/tools/definitions.hxx>
+#include <state-observation/tools/definitions.hxx>
 }
 
 #endif //STATEOBSERVATIONDEFINITIONSHPP
