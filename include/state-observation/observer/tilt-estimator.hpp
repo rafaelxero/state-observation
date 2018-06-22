2dc598ca875993da85608780b07ed2cd0b0d35d6
Rafael Cisneros <rafael.cisneros@aist.go.jp>
2018-06-22 18:27:36 +0900

    Completed the implementation of the tilt estimator, not tested yet

--- include/state-observation/observer/tilt-estimator.hpp
+++ include/state-observation/observer/tilt-estimator.hpp
@@ -32,26 +32,59 @@ namespace stateObservation
   {
   public:
 
+    /// The constructor
+    ///  \li alpha : parameter related to the convergence of the opposite of the
+    ///              linear velocity of the IMU expressed in the control frame
+    ///  \li beta  : parameter related to the convergence of the tilt
     TiltEstimator(unsigned alpha, unsigned beta);
 
+    void setAlpha(const unsigned alpha) { alpha_ = alpha; }
+    unsigned getAlpha() const { return alpha_; }
+    
+    void setBeta(const unsigned beta) { beta_ = beta; }
+    unsigned getBeta() const { return beta_; }
+
+    void setSamplingTime(const unsigned dt) { dt_ = dt; }
+    unsigned getSamplingTime() const { return dt_; }
+
+    void setSensorPositionInC(const Vector3& p) { p_S_C = p; }
+    Vector3 getSensorPositionInC() { return p_S_C; }
+    
+    void setSensorOrientationInC(const Matrix3& R) { R_S_C = R; }
+    Matrix3 getSensorOrientationInC() { return R_S_C; }
+    
+    void setSensorLinearVelocityInC(const Vector3& v) { v_S_C = v; }
+    Vector3 getSensorLinearVelocityInC() { return v_S_C; }
+    
+    void setSensorAngularVelocityInC(const Vector3& w) { w_S_C = w; }
+    Vector3 getSensorAngularVelocityInC() { return w_S_C; }
+    
+    void setMeasurement(const Vector ya_k, const Vector yg_k, TimeIndex k) override;
+    
   protected:
 
     /// The parameters of the estimator chosen such that
     /// beta_ * gravityConstant < alpha_^2
     unsigned alpha_, beta_;
 
-    /// The tilt estimator loop
-    virtual StateVector oneStepEstimation_();
+    /// Sampling time
+    unsigned dt_;
+    
+    /// Position of the IMU in the control frame
+    Vector3 p_S_C;
 
-    struct optimizationContainer
-    {
-      Vector y1;
-      Vector x1;
-      Vector dx1_hat;
-      Vector dx2_hat;
-    } oc_;
+    /// Orientation of the IMU in the control frame
+    Matrix3 R_S_C;
+
+    /// Linear velocity of the IMU in the control frame
+    Vector3 v_S_C;
+
+    /// Angular velocity of the IMU in the control frame
+    Vector3 w_S_C;
+    
+    /// The tilt estimator loop
+    StateVector oneStepEstimation_();
   };
-  
   
 }
 
--- src/tilt-estimator.cpp
+++ src/tilt-estimator.cpp
@@ -4,22 +4,40 @@ namespace stateObservation
 {
 
   TiltEstimator::TiltEstimator(unsigned alpha, unsigned beta)
-    : ZeroDelayObserver(6, 6), alpha_(alpha), beta_(beta)
+    : ZeroDelayObserver(6, 6), alpha_(alpha), beta_(beta),
+      p_S_C(Vector3::Zero()), R_S_C(Matrix3::Zero()), v_S_C(Vector3::Zero()), w_S_C(Vector3::Zero())
   {
+  }
+  
+  void TiltEstimator::setMeasurement(const Vector ya_k, const Vector yg_k, TimeIndex k)
+  {
+    ObserverBase::MeasureVector y_k;
+    y_k = ya_k, yg_k;
+
+    ZeroDelayObserver::setMeasurement(y_k, k);
   }
 
   ObserverBase::StateVector TiltEstimator::oneStepEstimation_()
   {
-
+    TimeIndex k = this->x_.getTime();
+    
     BOOST_ASSERT(this->y_.size() > 0 && this->y_.checkIndex(k+1) && "ERROR: The measurement vector is not set");
 
-    oc_.y1 = R_S_C * yg - omega_S_C;
-    oc_.x1 = p_S_C.cross(oc_.y1) - dp_S_C;
-
-    oc_.dx1_hat = oc_.x1_hat.cross(oc_.y1) + gravityConstant * oc_.x2_hat - R_S_C * ya + alpha_ * (oc_.x1 - oc_.x1_hat);
-    oc_.dx2_hat = oc_.x2_hat.cross(oc_.y1 - beta_ * oc_.x2_hat.cross(oc_.x1 - oc_.x1_hat));
+    Vector3 ya = getMeasurement(k+1).head(3);
+    Vecotr3 yg = getMeasurement(k+1).tail(3);
     
-    return oc_.x2_hat;
+    Vector3 y1 = R_S_C * yg - omega_S_C;
+    Vector3 x1 = p_S_C.cross(y1) - dp_S_C;
+
+    Vector6 dx_hat;
+    dx_hat.head(3) = x1_hat.cross(y1) + gravityConstant * x2_hat - R_S_C * ya + alpha_ * (x1 - x1_hat);
+    dx_hat.tail(3) = x2_hat.cross(y1 - beta_ * x2_hat.cross(x1 - x1_hat));
+    
+    ObserverBase::StateVector x_hat = getEstimatedState(k) + dx_hat * dt_;
+    
+    setState(x_hat, k+1);
+    
+    return x_hat.tail(3);
   }
 
   
